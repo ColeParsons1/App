d65e590344a932568996cc6948fa11c70477b5c4
ColeParsons1 <coleparsons22@gmail.com>
2022-11-21 19:34:51 -0500

    kjhf

--- main/serializers.py
+++ main/serializers.py
@@ -24,7 +24,8 @@ class JobSerializer(serializers.ModelSerializer):
 	def get_Job_Type(self, Job):
 		if Job.Job_Type:
 			return Job.Job_Type.Label
-		return default
+		else:	
+			return "General"
 	def get_Assigned_Lugger(self, Job):
 		if Job.Assigned_Lugger:
 			return Job.Assigned_Lugger.username
