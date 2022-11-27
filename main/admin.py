200c2f716d9e16fdc821712380abf2bcca3cdf0f
ColeParsons1 <coleparsons22@gmail.com>
2022-11-27 16:18:22 -0500

    drgfe

--- main/admin.py
+++ main/admin.py
@@ -14,10 +14,11 @@ from django.contrib.auth.models import User
 from django.contrib.admin.sites import AdminSite
 #from .models import Profile
 from API.models import API_Test
-from .models import Profile, Job, Topic, Notification, Message, Account_Type
+from .models import Profile, Job, Topic, Notification, Message, Account_Type, Images
 #from friendship.admin import 
 admin.site.register(Job)
 admin.site.register(Topic)
+admin.site.register(Images)
 #admin.site.register(Notification)
 admin.site.register(Message)
 #admin.site.register(Account_Type)
